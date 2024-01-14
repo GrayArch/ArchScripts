82f97716b76146ae0fb24ea4a42fa971a170b513
Red Teamer <108466862+GrayArch@users.noreply.github.com>
2024-01-14 22:24:47 +0300

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 clear
-echo "##############################################################"
-echo " __________                          .___                     "
-echo " \______   \_______  _________     __| _/____  ____   _____   "
-echo "  |    |  _/\_  __ \/  _ \__  \   / __ |/ ___\/  _ \ /     \  "
-echo "  |    |   \ |  | \(  <_> ) __ \_/ /_/ \  \__(  <_> )  Y Y  \ "
-echo "  |______  / |__|   \____(____  /\____ |\___  >____/|__|_|  / "
-echo "         \/                   \/      \/    \/            \/  "
-echo "                                                              "
-echo "##############################################################"
+echo" ██████  ██████   █████  ██    ██  █████  ██████   ██████ ██   ██"
+echo"██       ██   ██ ██   ██  ██  ██  ██   ██ ██   ██ ██      ██   ██"
+echo"██   ███ ██████  ███████   ████   ███████ ██████  ██      ███████"
+echo"██    ██ ██   ██ ██   ██    ██    ██   ██ ██   ██ ██      ██   ██"
+echo" ██████  ██   ██ ██   ██    ██    ██   ██ ██   ██  ██████ ██   ██" 
 sleep 2
 
 PS3='Please select your choice: '
@@ -18,7 +14,7 @@ select opt in "${options[@]}"
 do
     case $opt in
         "Install Wifi Driver")
-            echo "After Installation Complete Your Machine Will Reboot"
+            echo "After Installation Complete Your Machine Will Reboot To Finish The Setup"
             sudo pacman -Syu
             sudo pacman -S linux-headers broadcom-wl-dkms
             sudo modprobe -r b44 b43 b43legacy ssb brcmsmac bcma
